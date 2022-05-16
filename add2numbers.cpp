dbc724a493a6fe3f65fd8f64dcc31465f88d235a
ARSH BAJWA <arshbajwa534@gmail.com>
2022-05-16 22:24:13 +0530

    program of add2numbers is added

---
+++ add2numbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      int a=2;
+      int b=4;
+      cout<<"Sum of 2 numbers is "<<a+b<<endl;
+      cout<<"This program is made by Arshdeep kaur"<<endl;
+    return 0;
+}
